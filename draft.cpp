b8648eb9065c79ca96c0fde6e5006031d07087ea
Марат Зайнуллин <marat.zainullin.96@gmail.com>
2016-09-30 19:36:29 +0300

    sem4_ex1 - lists and structures

--- draft.cpp
+++ draft.cpp
@@ -4,14 +4,13 @@
 #include <iostream>
 
 int main() {
-    int first, second; // Числа, введенные с клавиатуры
-    std::cout << "Entre deux nubres: ";
-    //std::cin >> first >> second;
-    //std::cout << "!! " << first << "    AND    " << second << std::endl;
-    for (int i = 0; i < 0; i++)
-        std::cout << i;
-    for(int i = 0; i < 2; i++)
-        std::cout << i;
+    int x = 0x61fe2c;
+    int y = *x;
+    int z = 100;
+    int *ptrz = &z;
+
+
+    std::cout << z << " " << ptrz << " " << *ptrz << " " << x <<std::endl;
     return 0;
 }
 
--- sem4_ex1.cpp
+++ sem4_ex1.cpp
@@ -1,51 +1,77 @@
 #include <cstdlib>
 #include <iostream>
 
-struct Node {      //Структура являющаяся звеном списка
-    double x, y;     //Значение (x, y) будет передаваться в список
-    Node *next, *prev; //Указатели на адреса следующего и предыдущего элементов списка
+struct Node {                               //Создание типа данных Node(звено).
+    double x, y;                            //Данные звена.
+    Node *next, *prev;                      //Указатели на адреса следующего и предыдущего элементов списка(звеньев).
 };
 
 
-struct List { //Создаем тип данных Список
-    Node *head = NULL;
-    Node *tail;  //Указатели на адреса начала списка и его конца
+struct DynList {                            //Создание типа данных DynList(список).
+    Node *head;                             //Указатели на адреса начала и конца списка.
+    Node *tail;
 };
 
 
-void add(List *list, double x, double y) {
-    Node *temp = new Node(); // Выделение памяти под новый элемент структуры
-    temp->next = NULL;       // Указываем, что изначально по следующему адресу пусто
-    temp->x = x;             // Записываем значение в структуру
+DynList *create_DynList() {                 //Создание пустого списка.
+    DynList *list = new DynList;            //Выделение памяти под переменную типа DynList.
+    list->head = list->tail = nullptr;         //Голова и хвост ничто(их указатели указывают на NULL), но они существуют.
+    return list;
+}
+
+
+
+void delete_DynList(DynList *list) {
+    Node *temp;
+    temp = list->tail;
+    Node *prev;
+    while (temp != nullptr) {
+        prev = temp->prev;
+        delete temp;
+        temp = prev;
+    }
+    delete list;
+}
+
+
+
+void add_node(DynList *list, double x, double y) {
+    Node *temp;
+    temp = new Node();                // Выделение памяти под новый элемент структуры(звено).
+    temp->next = nullptr;                      // Указываем, что изначально по следующему адресу пусто
+    temp->x = x;                            // Записываем значение в структуру
     temp->y = y;
-    if (list->head != NULL) { // Если список не пуст
-        temp->prev = list->tail; // Указываем адрес на предыдущий элемент в соотв. поле
-        list->tail->next = temp; // Указываем адрес следующего за хвостом элемента
-        list->tail = temp;       //Меняем адрес хвоста
-    } else { //Если список пустой
-        temp->prev = NULL; // Предыдущий элемент указывает в пустоту
-        list->head = list->tail = temp;    // Голова=Хвост=тот элемент, что сейчас добавили
+    if (list->head != nullptr) {               // Если список не пуст
+        temp->prev = list->tail;            // Указываем адрес на предыдущий элемент в соотв. поле
+        list->tail->next = temp;            // Указываем адрес следующего за хвостом элемента
+        list->tail = temp;                  //Меняем адрес хвоста
+    } else {                                //Если список пустой
+        temp->prev = nullptr;                  // Предыдущий элемент указывает в пустоту
+        list->head = list->tail = temp;     // Голова=Хвост=тот элемент, что сейчас добавили
     }
 }
 
 
-void print(List *list) {
-    Node *temp = list->head;  // Временно указываем на адрес первого элемента
-    while (temp != NULL) {    // Пока не встретим пустое значение
-        std::cout << "(" << temp->x << " " << temp->y << "), "; //Выводим значение на экран
-        temp = temp->next;     //Смена адреса на адрес следующего элемента
+void print(DynList *list) {
+    Node *temp = list->head;                                         // Временно указываем на адрес первого элемента
+    while (temp != nullptr) {                                           // Пока не встретим пустое значение
+        std::cout << "(" << temp->x << "; " << temp->y << ") ";      //Выводим значение на экран
+        temp = temp->next;                                           //Смена адреса на адрес следующего элемента
     }
     std::cout << "\n";
 }
 
 
 int main() {
-    List lst;
-    lst.tail = NULL;
-    //lst->tail = NULL;
+    DynList *MyList = create_DynList();
+
     for (int i = 0; i < 20; i++) {
-        add(&lst, rand() % 100, rand() % 100);
+        add_node(MyList, rand() % 100, rand() % 100);
     }
-    print(&lst);
+    print(MyList);
+    std::cout << MyList;
+    delete_DynList(MyList);
+    print(MyList);
+    std::cout << MyList;
     return 0;
 }
