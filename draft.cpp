d9a7eda19cc6d897ffa60cdffa9d06a4cbeedaa8
Марат Зайнуллин <marat.zainullin.96@gmail.com>
2016-10-03 00:10:03 +0300

    sem4_ex1 - *

--- draft.cpp
+++ draft.cpp
@@ -1,16 +1,11 @@
-//
-// Created by Marat on 17.09.2016.
-//
 #include <iostream>
 
+
+
+
+
 int main() {
-    int x = 0x61fe2c;
-    int y = *x;
-    int z = 100;
-    int *ptrz = &z;
 
-
-    std::cout << z << " " << ptrz << " " << *ptrz << " " << x <<std::endl;
     return 0;
 }
 
