ef41c768bd6d44fcc9a6be5fb371b673e3d940dd
Марат Зайнуллин <marat.zainullin.96@gmail.com>
2016-11-19 07:47:39 +0300

    Done!

--- DimQ.cpp
+++ DimQ.cpp
@@ -1,6 +1,6 @@
 #include <typeinfo>
 #include <iostream>
-#include <string>
+#include <iomanip>
 #include "type.h"
 
 
@@ -50,40 +50,72 @@ public:
                 (value / right.get_val());
     };
 
+    template<int L1, int M1, int T1, int I1, int O1, int N1, int J1>
+    friend std::ostream &operator<<(std::ostream &out_stream,
+                                    const DimQ<L1, M1, T1, I1, O1, N1, J1> &);
+
+
 };
 
 
-
-
-
-
-typedef DimQ<0, 0, 0, 0, 0, 0, 0> scalar;
-typedef DimQ<1, 0, 0, 0, 0, 0, 0> length;
-typedef DimQ<0, 0, 1, 0, 0, 0, 0> time;
-typedef DimQ<1, 0, -1, 0, 0, 0, 0> speed;
-typedef DimQ<1, 0, -2, 0, 0, 0, 0> accelerate;
+template<int L, int M, int T, int I, int O, int N, int J>
+std::ostream &operator<<(std::ostream &out_stream, const DimQ<L, M, T, I, O, N, J> &V) {
+    out_stream << V.value << " ";
+    if (L) out_stream << "m^(" << L << ")";
+    if (M) out_stream << "kg^(" << M << ")";
+    if (T) out_stream << "s^(" << T << ")";
+    if (I) out_stream << "A^(" << I << ")";
+    if (O) out_stream << "K^(" << O << ")";
+    if (N) out_stream << "mol^(" << N << ")";
+    if (J) out_stream << "cd^(" << J << ")";
+    return out_stream;
+}
 
 
 int main() {
 
-    scalar sc = 100;
-    length l = 15;
-    time t = 5;
-    auto s = l / t;
-    auto a = l / t / t;
-    auto ad100 = a / sc;
-    auto lx100 = s * t * sc + l;
-    auto smth = sc * t / t + sc;
+    using namespace std;
+    typedef DimQ<0, 0, 0, 0, 0, 0, 0> Scalar;
+    typedef DimQ<1, 0, 0, 0, 0, 0, 0> Length;
+    typedef DimQ<0, 0, 1, 0, 0, 0, 0> Time;
+    typedef DimQ<1, 0, -1, 0, 0, 0, 0> Velocity;
+    typedef DimQ<1, 0, -2, 0, 0, 0, 0> Acceleration;
 
 
-    std::cout << "l: " << l.get_val() << " " << type(l) << "\n"
-              << "t: " << t.get_val() << " " << type(t) << "\n"
-              << "s: " << s.get_val() << " " << type(s) << "\n"
-              << "a: " << a.get_val() << " " << type(a) << "\n"
-              << "a/100: " << ad100.get_val() << " " << type(ad100) << "\n"
-              << "l*100 + l: " << lx100.get_val() << " " << type(lx100) << "\n"
-              << "sc * t / t + sc : " << smth.get_val() << " " << type(smth) << "\n";
+    Scalar sc = 100;
+    Length l1 = 15;
+    Time t1 = 5;
+    auto s = l1 / t1;
+    auto a1 = l1 / t1 / t1;
+    auto ad100 = a1 / sc;
+    auto lx100 = s * t1 * sc + l1;
+    auto smth = sc * t1 / t1 + sc;
 
+
+    //std::cout.setf(std::ios::left);
+    cout << setw(35) << left << type(l1) << setw(10) << l1  << "\n"
+         << setw(35) << left << type(t1) << setw(10) << t1  << "\n"
+         << setw(35) << left << type(s) << setw(10) << s  << "\n"
+         << setw(35) << left << type(a1) << setw(10) << a1  << "\n"
+         << setw(35) << left << type(ad100) << setw(10) << ad100  << "\n"
+         << setw(35) << left << type(lx100) << setw(10) << lx100  << "\n"
+         << setw(35) << left << type(smth) << setw(10) << smth  << "\n\n\n";
+
+
+
+
+    Length l = {100};
+    Time t = {20};
+    Velocity v = l / t;
+    Acceleration a = v / t;
+
+    auto smth1 = v*a*a/t;
+    auto dimensionless = v/v;
+
+    cout << v << endl;
+    cout << a << endl;
+    cout << smth1 << endl;
+    cout << dimensionless << endl;
 }
 
 
